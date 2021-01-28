62c4023ea9fee54b8ca3f1faa61ec5df02701ba9
FernandoCarranza <fernandocarranza124@gmail.com>
2021-01-28 12:19:40 -0600

    Actualizacion

--- lectura.py
+++ lectura.py
@@ -4,6 +4,7 @@ import random
 import datetime
 import hashlib
 import sched, time
+import mysql.connector
 
 
 def insertarPulso(idSensor, sensorKey,hora):
@@ -41,7 +42,7 @@ def insertarRespiracion(idSensor, sensorKey,hora):
     #threading.Timer(5.0, insertarTemperatura(sensorKey)).start()
     
 
-    valor = random.randint(30, 70)    
+    valor = (random.randint(60, 140))/2
     
     firma = sensorKey+hora
     encript = (hashlib.md5(firma.encode())).hexdigest()
@@ -60,7 +61,27 @@ def ciclo(claves = []):
     insertarRespiracion(3,claves[2],hora)
     s.enter(10, 1, ciclo, (claves,))
     
-claves = ["FFDHT11TEST", "GGHPULSOCARCTEST","HHRESPIRATEST"]
+#claves = ["FFDHT11TEST", "GGHPULSOCARCTEST","HHRESPIRATEST"]
+def obtieneclaves():
+    mydb = mysql.connector.connect (
+    host="localhost",
+    user="root",
+    password="password",
+    database="monitorbebefirmas"
+    )
+    array = []
+    mycursor = mydb.cursor()
+    mycursor.execute("select * from sensor")
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        array.append(x[1])
+        
+    return array
+
+claves = obtieneclaves()
+print (claves)
+
+
 s = sched.scheduler(time.time, time.sleep)
 s.enter(10,1, ciclo,(claves,))
 s.run()
